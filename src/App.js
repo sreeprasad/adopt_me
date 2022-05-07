555a4a4543fa8573db74d4969915a5817ffffdad
Argan Oil <arganoil@Argans-MacBook-Pro.local>
2022-05-07 12:41:25 -0700

    configure eslint with jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Pet from './Pet.js'
+import { render } from "react-dom";
+import Pet from "./Pet.js";
 
 const App = () => {
   return (
@@ -10,22 +9,6 @@ const App = () => {
       <Pet name="Twitter" animal="bird" breed="parrot" />
       <Pet name="Bond" animal="cat" breed="mixed" />
     </div>
-  )
-}
-/*
-const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { id: 'brand' }, 'Adopt Me!'),
-    React.createElement(Pet, { name: 'Tiger', animal: 'Dog', breed: 'Husky' }),
-    React.createElement(Pet, {
-      name: 'Twitter',
-      animal: 'Bird',
-      breed: 'Parrot'
-    }),
-    React.createElement(Pet, { name: 'Bond', animal: 'Cat', breed: 'Mixed' })
-  ])
-}
-
-render(React.createElement(App), document.getElementById('root'))
-*/
-render(<App />, document.getElementById('root'))
+  );
+};
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,25 +1,14 @@
-import React from 'react'
+import React from "react";
 
-// Pet component. Same as Pet component below.
-const Pet = props => {
+// Pet component.
+const Pet = (props) => {
   return (
     <div>
       <h1>{props.name}</h1>
       <h2>{props.animal}</h2>
       <h2>{props.breed}</h2>
-     </div>
-  )
-}
+    </div>
+  );
+};
 
-// pet component
-/*
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed)
-  ])
-}
-*/
-
-export default Pet
+export default Pet;

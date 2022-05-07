46823ee2cf4bd7e48e23f79ce6c191dc13a2583d
Argan Oil <arganoil@Argans-MacBook-Pro.local>
2022-05-07 09:47:03 -0700

    add prettier and eslint

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # adopt_me
+
+### Commands I ran on terminal
+
+1. -y option adds standard options without needing to fill in details
+   npm init -y
+
+2. Install prettier and immediately apply on file
+   npx prettier src/App.js --write
+
+3. -D indicates dev dependency and not prod dep
+   npm install -D prettier
+
+4. Automatically fix eslint error
+   npx run lint -- fix
+
+5. npm init @eslint/config
--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+/* global React ReactDOM */
 // pet component
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]);
-};
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed)
+  ])
+}
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {id:"brand"}, "Adopt Me!"),
-            React.createElement(Pet, {name:"Tiger", animal:"Dog", breed:"Husky"}),
-            React.createElement(Pet, {name:"Twitter", animal:"Bird", breed:"Parrot"}),
-            React.createElement(Pet, {name:"Bond", animal:"Cat", breed:"Mixed"}),
-        ]
-
-    );
-};
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+  return React.createElement('div', {}, [
+    React.createElement('h1', { id: 'brand' }, 'Adopt Me!'),
+    React.createElement(Pet, { name: 'Tiger', animal: 'Dog', breed: 'Husky' }),
+    React.createElement(Pet, {
+      name: 'Twitter',
+      animal: 'Bird',
+      breed: 'Parrot'
+    }),
+    React.createElement(Pet, { name: 'Bond', animal: 'Cat', breed: 'Mixed' })
+  ])
+}
+ReactDOM.render(React.createElement(App), document.getElementById('root'))

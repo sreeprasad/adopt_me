6abc25788bb853d53246ce074badfdd476965e4a
Argan Oil <arganoil@Argans-MacBook-Pro.local>
2022-05-07 23:33:32 -0700

    add react hooks

--- README.md
+++ README.md
@@ -25,5 +25,39 @@
 
 9. npm install -D eslint-plugin-import@2.25.4 eslint-plugin-jsx-a11y@6.5.1 eslint-plugin-react@7.28.0
 
-10 To fix vulnerabilities
-npm audit --fix
+10. To fix vulnerabilities
+    npm audit --fix
+
+11. use defaultValue in input forms to make values mutable
+    <input id="location" defaultValue={location} placeholder="Location" />
+
+12. useState depends on order of call. Have to call in same order.
+    Do not have conditional hook.
+    if (something) {
+    const [chair, setChair] = useState("")
+    }
+    const [animal, setAnimal] = useState(")
+    Depending on if something is True or not, animal may get chair value.
+    Dont put hook inside if conditions or rules.
+
+13. setLocation is variable. Can use updateLocation as well.
+
+14. controlled form. Used when input should be controlled. eg. valid password chars is present.
+<form>
+               <label htmlFor="location">
+                   Location  
+                   <input id="location" value={location} placeholder="Location" onChange={(e) => setLocation(e.target.value)} />
+               </label>
+               <button>Submit</button>
+           </form>
+
+15. uncontrolled form
+<form onSubmit={}>
+                <label htmlFor="location">
+                    Location  
+                    <input id="location" placeholder="Location" />
+                </label>
+                <button>Submit</button>
+            </form>
+
+16. npm install -D eslint-plugin-react-hooks@4.3.0
---
+++ src/SearchParams.js
@@ -0,0 +1,72 @@
+import { useState } from "react/cjs/react.development";
+
+const ANIMALS = ["bird", "dogs", "cat", "rabbit", "elephant"];
+
+const SearchParams = () => {
+  //const location = "Sunnyvale CA"
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
+  return (
+    <div className="search-params">
+      <form on>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed();
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed();
+            }}
+          >
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+              setBreed();
+            }}
+          >
+            {breeds.map((allbreed) => (
+              <option key={allbreed} value={allbreed}>
+                {allbreed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
